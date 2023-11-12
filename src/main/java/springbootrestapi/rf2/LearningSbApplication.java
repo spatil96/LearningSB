a719a450ed2ae1370e75f57836fcda16817d32b7
Sumeet Patil <spatil0296@gmail.com>
2023-11-12 09:56:48 -0500

    1

---
+++ src/main/java/springbootrestapi/rf2/LearningSbApplication.java
@@ -0,0 +1,13 @@
+package springbootrestapi.rf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springbootrestapi/rf2/LearningSbApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootrestapi.rf2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
